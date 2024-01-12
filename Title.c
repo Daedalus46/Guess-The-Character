e40da39bc654a8c1e03650d06938e565ed744f05
Daedalus46 <musman4012005@gmail.com>
2024-01-13 03:14:51 +0500

    Header added

---
+++ Title.c
@@ -0,0 +1,117 @@
+void border() {
+    for(int i = 0 ; i<=123 ; i++){
+    	printf("~");
+	}
+    printf("\n");
+}
+
+void Title() {
+	printf("\n");
+    printf("\t\t\t\t");
+    printf("\t $$$$$\t"); // G
+    printf(" $     $"); // U
+    printf("  $$$$$$"); // E
+    printf("   $$$$$$"); // S
+    printf("   $$$$$$\n"); // S
+    printf("\t\t\t\t");
+    printf("\t$\t"); // G
+    printf(" $     $"); // U
+    printf("  $\t"); // E
+    printf("  $  "); // S
+    printf("      $ \n"); // S
+    printf("\t\t\t\t");
+    printf("\t$   $$$\t"); // G
+    printf(" $     $"); // U
+    printf("  $$$$$$ "); // E
+    printf("  $$$$$$ "); // S
+    printf("  $$$$$$ \n"); // S
+    printf("\t\t\t\t");
+    printf("\t$     $\t"); // G
+    printf(" $     $"); // U
+    printf("  $\t"); // E
+    printf("         $ "); // S
+    printf("       $\n"); // S
+    printf("\t\t\t\t");
+    printf("\t $$$$$\t"); // G
+    printf("  $ $ $"); // U
+    printf("   $$$$$$ "); // E
+    printf("  $$$$$$ "); // S
+    printf("  $$$$$$ \n"); // S
+    
+    border();
+}
+
+void Title2() {
+    printf("\n");
+    printf("\t\t\t\t");
+    printf("\t\t   $$$$$$$ ");//T
+    printf("  $     $  ");//H
+    printf("  $$$$$$\n"); // E
+    printf("\t\t\t\t");
+    printf("\t\t      $  ");//T
+    printf("    $     $");//H
+    printf("   $\t\n"); // E
+    printf("\t\t\t\t");
+    printf("\t\t      $ ");//T
+    printf("     $$$$$$$ ");//H
+    printf("   $$$$$$ \n"); // E
+    printf("\t\t\t\t");  
+    printf("\t\t      $  ");//T
+    printf("    $     $");//H
+    printf("   $\t\n"); // E
+    printf("\t\t\t\t");
+    printf("\t\t      $  ");//T
+    printf("    $     $");//H
+    printf("    $$$$$$ \n"); // E
+    
+    border();
+}
+
+void Title3() {
+    printf("\n");
+    printf("\t\t\t\t");
+    printf("\t $$$$$$");//P
+    printf("   $$$$$$  "); // E
+    printf("$$$$$$");//R
+    printf("   $$$$$$"); // S
+    printf("   $$$$$$");//O
+    printf("   $      $   \n"); //N
+    printf("\t\t\t\t");
+    printf("\t $    $");//P
+    printf("  $\t  "); // E
+    printf("$    $ ");//R
+    printf(" $  "); // S
+    printf("\t   $      $");//O
+    printf("  $ $    $   \n");//N
+    printf("\t\t\t\t");
+    printf("\t $$$$$$");//P
+    printf("   $$$$$$  "); // E
+    printf("$$$$$$");//R
+    printf("   $$$$$$ "); // S
+    printf(" $      $");//O
+    printf("  $   $  $   \n");//N
+    printf("\t\t\t\t");
+    printf("\t $ ");//P
+    printf(" \t $  "); // E
+    printf("\t  $  $  ");//R
+    printf("\t $"); // S
+    printf(" $      $");//O
+    printf("  $    $ $   \n");//N
+    printf("\t\t\t\t");
+    printf("\t $  ");//P
+    printf("\t  $$$$$$ "); // E
+    printf(" $   $");//R
+    printf("    $$$$$$ "); // S
+    printf("  $$$$$$");//O
+    printf("   $      $   \n");//N
+}
+void Disclaimer( )
+{
+    printf("\n");
+    printf("    ****** DISCLAIMER ****** DISCLAIMER ****** DISCLAIMER ****** DISCLAIMER ****** DISCLAIMER ****** DISCLAIMER ****** DISCLAIMER ****** \n");
+}
+void Rules()
+{
+    printf("\t\t\t\t\t\t   R****U****L****E****S \n");
+    printf("\t\t\t\t *** This Game Contains Only Politician Leaders Of Pakistan ***");    
+}
