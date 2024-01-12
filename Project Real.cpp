8c91ae556f4f6121c57e9c8fbde73281fc35556a
Daedalus46 <musman4012005@gmail.com>
2024-01-13 02:47:22 +0500

    bilawal added

--- Project Real.cpp
+++ Project Real.cpp
@@ -41,6 +41,8 @@ void Start_the_Game() {
 		                    switch(ch) {
 								case 'Y':
 								case 'y': // Nawaz Sharif Q1
+								case 'D':
+								case 'd':
 								printf("\n\tQuestion no %d",count);
 								printf("\nIs your character over 50 years?\n");
 								printf("\t Option: ");
@@ -48,7 +50,9 @@ void Start_the_Game() {
 								count++;
 				            	switch(ch) {
 			                    	case 'Y':
-					                case 'y': // Nawaz Sharif Q2
+					                case 'y':
+									case 'D':
+									case 'd': // Nawaz Sharif Q2
 					                	printf("\n\tQuestion no %d",count);
 					                    printf("\nDoes your character have famous a brother?\n");
 					                    printf("\t Option: ");
@@ -56,7 +60,9 @@ void Start_the_Game() {
 					                    count++;
 					                    switch(ch) {
 					                    	case 'Y':
-							                case 'y': // Nawaz Sharif Q3
+							                case 'y':
+											case 'D':
+											case 'd': // Nawaz Sharif Q3
 							                	printf("\n\tQuestion no %d",count);
 							                    printf("\nDoes your character have famous a Daughter?\n");
 							                    printf("\t Option: ");
@@ -64,7 +70,9 @@ void Start_the_Game() {
 							                    count++;
 							                    switch(ch) {
 							                    	case 'Y':
-									                case 'y': // Nawaz Sharif Q4
+									                case 'y':
+													case 'D':
+													case 'd': // Nawaz Sharif Q4
 									                	printf("\n\tQuestion no %d",count);
 									                    printf("\nDoes your character's Daughter a politician?\n");
 									                    printf("\t Option: ");
@@ -76,7 +84,7 @@ void Start_the_Game() {
 											                case 'D':
 															case 'd':
 											                	printf("\n\tQuestion no %d",count);
-											                    printf("\nDoes your character introduced Green Line Project?\n");
+											                    printf("\nDoes your character introduced Projects?\n");
 											                    printf("\t Option: ");
 											                    scanf(" %c", &ch);
 											                    count++;
@@ -92,7 +100,9 @@ void Start_the_Game() {
 													                    count++;
 													                     switch(ch) {
 													                    	case 'Y':
-															                case 'y': // Nawaz Sharif Q7
+															                case 'y':
+																			case 'D':
+																			case 'd': // Nawaz Sharif Q7
 															                	printf("\n\tQuestion no %d",count);
 															                    printf("\nDid your character become PM more than 1 times?\n");
 															                    printf("\t Option: ");
@@ -100,7 +110,9 @@ void Start_the_Game() {
 															                    count++;
 															                    switch(ch) {
 															                    	case 'Y':
-																	                case 'y': // Nawaz Sharif Q8
+																	                case 'y':
+																					case 'D':
+																					case 'd': // Nawaz Sharif Q8
 																	                	printf("\n\tQuestion no %d",count);
 																	                    printf("\nIs your character half bald?\n");
 																	                    printf("\t Option: ");
@@ -108,7 +120,9 @@ void Start_the_Game() {
 																	                    count++;
 																	                    switch(ch) {
 																	                    	case 'Y':
-																			                case 'y': // Nawaz Sharif Q9
+																			                case 'y':
+																							case 'D':
+																							case 'd': // Nawaz Sharif Q9
 																			                	printf("\n\tQuestion no %d",count);
 																			                    printf("\nDoes he have a palace??\n");
 																			                    printf("\t Option: ");
@@ -120,7 +134,7 @@ void Start_the_Game() {
 																					                case 'D':
 																									case 'd':
 																					                	printf("\n\tQuestion no %d",count);
-																					                    printf("\nDoes your character look like a bhalu?\n");
+																					                    printf("\nDoes your character look like someone?\n");
 																					                    printf("\t Option: ");
 																					                    scanf(" %c", &ch);
 																					                    count++;
@@ -136,7 +150,9 @@ void Start_the_Game() {
 																							                    count++;
 																							                    switch(ch) {
 																							                    	case 'Y':
-																									                case 'y': // Nawaz Sharif Q12
+																									                case 'y':
+																													case 'D':
+																													case 'd': // Nawaz Sharif Q12
 																									                	printf("\n\tQuestion no %d",count);
 																									                    printf("\nDoes your character went for the medical abroad?\n");
 																									                    printf("\t Option: ");
@@ -144,7 +160,9 @@ void Start_the_Game() {
 																									                    count++;
 																									                    switch(ch) {
 																									                    	case 'Y':
-																											                case 'y': // Nawaz Sharif Q13
+																											                case 'y':
+																															case 'D':
+																															case 'd': // Nawaz Sharif Q13
 																											                	printf("\n\tQuestion no %d",count);
 																											                    printf("\nDid your character ran from the country?\n");
 																											                    printf("\t Option: ");
@@ -152,7 +170,9 @@ void Start_the_Game() {
 																											                    count++;
 																											                    	switch(ch) {
 																												                    	case 'Y':
-																														                case 'y': // Nawaz Sharif Q14
+																														                case 'y':
+																																		case 'D':
+																																		case 'd': // Nawaz Sharif Q14
 																														                	printf("\n\tQuestion no %d",count);
 																														                    printf("\nIs your character's wife alive?\n");
 																														                    printf("\t Option: ");
@@ -171,6 +191,8 @@ void Start_the_Game() {
 																																			        switch(ch) {
 																																			            case 'Y':
 																																					    case 'y':
+																																					    case 'D':
+																																						case 'd':
 																																		                	printf("\nYour character is the one and only nawaz shariff!\n");
 																																		                	printf("\n\tna sadiq hu na Ameen hu \n\tme krapshan ki masheen hu");
 																						            														}
@@ -216,7 +238,216 @@ void Start_the_Game() {
 					                default:// Nawaz Sharif Q2
 					                	break;
 								}
-								default: // Nawaz Sharif Q1
+									 // Nawaz Sharif Q1
+								default: 
+									printf("\n\tQuestion no %d\n",count);  // bilawal bhutto Q1
+									printf("Is your character alive?\n");
+									printf("\t Option: ");   
+									scanf(" %c", &ch);
+									count++;
+									switch(ch) {
+										case 'Y':// bilawal bhutto Q2
+									  	case 'y':
+										case 'D':
+										case 'd':
+											printf("\n\tQuestion no %d\n",count);
+											printf("Is his father or mother famous politician?\n");
+											printf("\t Option: ");    
+											scanf(" %c", &ch);
+											count++;
+										switch(ch) {
+											case 'Y':// bilawal bhutto Q3
+										  	case 'y':
+											case 'D':
+											case 'd':
+												printf("\n\tQuestion no %d\n",count);
+												printf("Is your character clean-shaved?\n");
+												printf("\t Option: ");    
+												scanf(" %c", &ch);
+												count++;
+												switch(ch) {
+													case 'Y':// bilawal bhutto Q4
+												  	case 'y':
+													case 'D':
+													case 'd':
+														printf("\n\tQuestion no %d\n",count);
+														printf("Is your character un-married?\n");
+														printf("\t Option: ");    
+														scanf(" %c", &ch);
+														count++;
+													switch(ch) {
+														case 'Y':// bilawal bhutto Q5
+														case 'y':
+														case 'D':
+														case 'd':
+															printf("\n\tQuestion no %d\n",count);
+															printf("Your character studied abroad?\n");
+															printf("\t Option: ");    
+															scanf(" %c", &ch);
+															count++;
+															switch(ch) {
+																case 'Y':// bilawal bhutto Q6
+																case 'y':
+																case 'D':
+																case 'd':
+																	printf("\n\tQuestion no %d\n",count);
+																	printf("Did your character made visa free for all Irani people?\n");
+																	printf("\t Option: ");    
+																	scanf(" %c", &ch);
+																	count++;
+																	switch(ch) {
+																		case 'Y':// bilawal bhutto Q7
+																		case 'y':
+																		case 'D':
+																		case 'd':
+																			printf("\n\tQuestion no %d\n",count);
+																			printf("Does your politician holds higher position in party?\n");
+																			printf("\t Option: ");    
+																			scanf(" %c", &ch);
+																			count++;
+																			switch(ch) {
+																				case 'Y':// bilawal bhutto Q8
+																				case 'y':
+																				case 'D':
+																				case 'd':
+																					printf("\n\tQuestion no %d\n",count);
+																					printf("Was your politician a former prime minister/chief minister?\n");
+																					printf("\t Option: ");    
+																					scanf(" %c", &ch);
+																					count++;
+																					switch(ch) {
+																						case 'Y':// bilawal bhutto Q9
+																						case 'y':
+																						case 'D':
+																						case 'd':
+																							printf("\n\tQuestion no %d\n",count);
+																							printf("Does his speech not make any sense right?\n");
+																							printf("\t Option: ");    
+																							scanf(" %c", &ch);
+																							count++;
+																							switch(ch) {
+																								case 'Y':// bilawal bhutto Q10
+																								case 'y':
+																								case 'D':
+																								case 'd':
+																									printf("\n\tQuestion no %d\n",count);
+																									printf("Is he fluent in english??\n");
+																									printf("\t Option: ");    
+																									scanf(" %c", &ch);
+																									count++;
+																									switch(ch) {
+																										case 'Y':// bilawal bhutto Q11
+																										case 'y':
+																										case 'D':
+																										case 'd':
+																											printf("\n\tQuestion no %d\n",count);
+																											printf("Does he have issue in gender pronounciation?\n");
+																											printf("\t Option: ");    
+																											scanf(" %c", &ch);
+																											count++;
+																											switch(ch) {
+																												case 'Y':// bilawal bhutto Q12
+																												case 'y':
+																												case 'D':
+																												case 'd':
+																													printf("\n\tQuestion no %d\n",count);
+																													printf("Is he favorite of any other politician?\n");
+																													printf("\t Option: ");    
+																													scanf(" %c", &ch);
+																													count++;
+																													switch(ch) {
+																														case 'Y':// bilawal bhutto Q13
+																														case 'y':
+																														case 'D':
+																														case 'd':
+																															printf("\n\tQuestion no %d\n",count);
+																															printf("Did he recently said a province KFC?\n");
+																															printf("\t Option: ");    
+																															scanf(" %c", &ch);
+																															count++;
+																															switch(ch) {
+																																case 'Y':// bilawal bhutto Q14
+																																case 'y':
+																																case 'D':
+																																case 'd':
+																																	printf("\n\tQuestion no %d\n",count);
+																																	printf("Kya aap k politician ka bolne ka andaaz thora kasual he?\t\tkiya apki kanpe tang rahi hai\n");
+																																	printf("\t Option: ");    
+																																	scanf(" %c", &ch);
+																																	count++;
+																																	switch(ch) {
+																																		case 'Y':// bilawal bhutto Q15
+																																		case 'y':
+																																		case 'D':
+																																		case 'd':
+																																			printf("\n\tQuestion no %d\n",count);
+																																			printf("Is your politician likely to become Prime minister in future?\n");
+																																			printf("\t Option: ");    
+																																			scanf(" %c", &ch);
+																																			count++;
+																																			switch(ch) {
+																																				case 'Y':// bilawal bhutto Q16
+																																				case 'y':
+																																				case 'D':
+																																				case 'd':
+																																					printf("\n\tQuestion no %d\n",count);
+																																					printf("You know him because of (Barish huti hai tw pani ata hai)?\n");
+																																					printf("\t Option: ");    
+																																					scanf(" %c", &ch);
+																																					count++;
+																																					switch(ch) {
+																																						case 'Y':// bilawal bhutto end
+																																						case 'y':
+																																						case 'D':
+																																						case 'd':
+																																							printf("\nYour character is Bilawal Bhutto!\n");
+																																							printf("\ntum to aik kanputli (statue) ho\ntumhari doren to PF hilaa raha hai");
+																																					}
+																																				default:
+																																					break;
+																																			}
+																																		default:
+																																			break;
+																																	}
+																																default:
+																																	break;
+																															}
+																														default:
+																															break;
+																													}
+																												default:
+																													break;
+																											}
+																										default:
+																											break;
+																									}
+																								default:
+																									break;
+																							}
+																						default:
+																							break;
+																					}
+																				default:
+																					break;
+																			}
+																		default:
+																			break;
+																	}
+																default:
+																	break;
+															}
+														default:
+															break;
+													}
+													default:
+														break;
+												}
+											default:
+												break;
+										}
+										default:
+											break;
+									}		
 									break;
 			            }
 		                default:
